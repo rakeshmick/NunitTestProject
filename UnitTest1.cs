3e5ad3b4837c897efbfd16c8a4ad49ea7de6b599
Rakesh Prabhakaran <rakesh.1985march@gmail.com>
2021-08-13 16:30:57 +1000

    first commit

---
+++ UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace NunitTestProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.IsTrue(false);
+        }
+        [Test]
+        public void Test2()
+        {
+            Assert.Pass();
+        }
+        [Test]
+        public void Test3()
+        {
+            Assert.Pass();
+        }
+    }
+}
